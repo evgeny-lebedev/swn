96a2ecc2756984a3ad0c38a12050c1c4f4d7b856
Evgeny Lebedev <evgeny.lebedev1110@gmail.com>
2019-07-09 23:33:06 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/swn/precache-manifest.2967000e271571d12d1525b54c60449a.js"
+  "/swn/precache-manifest.d07fa63bcd8c9af5431b0ffe69127639.js"
 );
 
 self.addEventListener('message', (event) => {
