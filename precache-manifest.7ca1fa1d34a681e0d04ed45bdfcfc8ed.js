0733b1224def00101d642758206584ee60c3eabb
Evgeny Lebedev <evgeny.lebedev1110@gmail.com>
2019-07-09 23:05:35 +0300

    Updates

---
+++ precache-manifest.7ca1fa1d34a681e0d04ed45bdfcfc8ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50061e2c51fc3f96c0cdc40113b431f7",
+    "url": "/swn/index.html"
+  },
+  {
+    "revision": "3f6e7e7a1dd0f995e660",
+    "url": "/swn/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5665491d87d13406a4dd",
+    "url": "/swn/static/js/2.0497e0bd.chunk.js"
+  },
+  {
+    "revision": "3f6e7e7a1dd0f995e660",
+    "url": "/swn/static/js/main.bd621c9a.chunk.js"
+  },
+  {
+    "revision": "a740c1c1a9d62eff4402",
+    "url": "/swn/static/js/runtime~main.04c42d69.js"
+  }
+]);
